3bbeaa7781dd95c452e10b14bb9bcd04125bce06
Yunus Yavuz <yunusyavuz016@gmail.com>
2022-12-05 22:28:02 +0300

    recess apiql

---
+++ appointmentApiQL/Models/User/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Recess.ApiQL.Models.User
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Bio { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public string ProfileBannerUrl { get; set; }
+        public string EmailAddress { get; set; }
+        public Dictionary<string, string> Links { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiration { get; set; }
+    }
+}
